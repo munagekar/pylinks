0c99941249a7662a3109e80cfdf6178f1aa8d04a
munagekar <avm.abhishek@gmail.com>
2020-03-20 07:09:44 +0900

    Add Team Roles

--- pylinks/app.py
+++ pylinks/app.py
@@ -1,6 +1,8 @@
 import logging
 import sqlite3
 from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 
 import databases
 import sqlalchemy
@@ -8,20 +10,35 @@ from fastapi import FastAPI, HTTPException, Path, status
 from pydantic import BaseModel
 
 logging.basicConfig()
-logger = logging.Logger(__name__)
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
 class User(BaseModel):
-    id: int
+    id: Optional[int] = None
     username: str
-    created: datetime
+    created: Optional[datetime] = None
 
 
 class Team(BaseModel):
-    id: int
+    id: Optional[int] = None
     teamname: str
-    created: datetime
+    created: Optional[datetime] = None
+
+
+class UserRole(str, Enum):
+    READER = "reader"
+    WRITER = "writer"
+    ADMIN = "admin"
+
+
+USER_ROLE_MAP = {UserRole.READER: 0, UserRole.WRITER: 1, UserRole.ADMIN: 1}
+
+
+class TeamRole(BaseModel):
+    user: User
+    team: Team
+    role: UserRole = UserRole.READER
 
 
 DATABASE_URL = "sqlite:///data/test.db"
@@ -43,6 +60,14 @@ teams = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("teamname", sqlalchemy.String(25), unique=True, index=True, nullable=False),
     sqlalchemy.Column("created", sqlalchemy.DATETIME, nullable=False),
+)
+
+team_roles = sqlalchemy.Table(
+    "team_roles",
+    metadata,
+    sqlalchemy.Column("team_id", sqlalchemy.Integer, index=True, nullable=False),
+    sqlalchemy.Column("user_id", sqlalchemy.Integer, index=True, nullable=False),
+    sqlalchemy.Column("role_id", sqlalchemy.Integer, default=USER_ROLE_MAP[UserRole.READER]),
 )
 
 engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
@@ -79,3 +104,15 @@ async def create_user(username: str = Path(..., max_length=25)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username Not Unique")
 
     return User(id=user_id, username=username, created=created)
+
+
+@app.post("/team/{team_name}", responses={400: {"detail": "Teamname Not Unique"}})
+async def create_team(*, team_name: str = Path(..., max_length=25), team_roles: List[TeamRole]):
+    try:
+        created = datetime.utcnow()
+        query = teams.insert.values(teamname=team_name, created=created)
+        team_id = await database.execute(query)
+        logger.info("Created New Team. Team:%s Id:%s", team_name, team_id)
+    except sqlite3.IntegrityError:
+        logger.info("Failed to Create New Team. Team:%s already exists", team_name)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Teamname Not Unique")
