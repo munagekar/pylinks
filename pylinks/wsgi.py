eeb3aabaac5f3408bd60cd3d5ebed869ee166c14
munagekar <avm.abhishek@gmail.com>
2020-03-23 19:05:19 +0900

    Seperate out run funciton

--- pylinks/wsgi.py
+++ pylinks/wsgi.py
@@ -2,5 +2,10 @@ import uvicorn  # type: ignore
 
 from pylinks.app import app
 
-if __name__ == "__main__":
+
+def run():
     uvicorn.run(app, host="0.0.0.0", interface="wsgi", port=80, log_level="info")
+
+
+if __name__ == "__main__":
+    run()
