63bb6883c25dda37d274ce9ea8222e84043f0bed
munagekar <avm.abhishek@gmail.com>
2020-03-23 16:29:02 +0900

    Lint

--- pylinks/config.py
+++ pylinks/config.py
@@ -4,3 +4,7 @@ import environ  # type: ignore
 @environ.config()
 class Env:
     key = environ.var(name="SECRET_KEY", help="Secret key")
+
+
+def read_from_env() -> Env:
+    return environ.to_config(Env)
